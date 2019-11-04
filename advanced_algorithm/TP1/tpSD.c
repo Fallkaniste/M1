c52eb46849d302fe8322f58eca52fdcb8694a951
amiard landry <mrl2714a@204-07l.ens.fsi>
2019-11-04 18:22:30 +0100

    advances_algorithtm_part2

--- advanced_algorithm/TP1/tpSD.c
+++ advanced_algorithm/TP1/tpSD.c
@@ -62,7 +62,7 @@ void percolate_down(int *array, int size, int i) {
 	int m;
 	int l = left(i);
 	int r = right(i);
-	if ((l <= size) && (array[i-1] < array[r-1])) {
+	if ((l <= size) && (array[i-1] < array[l-1])) {
 		m = l;
 	}
 	else {
@@ -77,12 +77,28 @@ void percolate_down(int *array, int size, int i) {
 	}
 }
 
+void percolate_up(int *array, int i) {
+	int j = parent(i);
+	while ((j > 0) && (array[j-1] < array[i-1])) {
+		swap(&array[i-1], &array[j-1]);
+		i = j ;
+		j =parent(i);
+	}
+
+}
+
 int pop(int *array, int *size) {
 	int m = array[0];
 	array[0] = array[*size-1];
  	*size = *size -1;
 	percolate_down(array, *size, 1);
 	return m;
+}
+
+void add(int *array, int *size, int elem){
+	*size=*size+1;
+	array[*size-1]=elem;
+	percolate_up(array, *size);
 }
 
 void build_heap(int *array, int size) {
@@ -109,16 +125,26 @@ int main( int argc, char **argv ) {
 
 	/* read the data from the file given in arg or from stdin */
 	read_data( f_in, &data, &n, &k );
-	//bubble_sort(data,n,k);
+	bubble_sort(data,n,k);
+	int d=0;
+	int test[n+5];
+	for (size_t i = 0; i < n; i++) {
+		add(test,&d,data[i]);
+	}
+	for (size_t i = 0; i < k; i++) {
+		pop(test,&n);
+	}
+	print_data(test,n);
 	build_heap(data,n);
-
-	pop(data,&n);
 
 	/* print the array */
 	print_data(data,n);
 	printf("K : %d\n", k);
 	printf("D : %d\n", n);
-
+/*	for (size_t i = k; i >0 ; i--) {
+		printf("%d\n",data[0]);
+		pop(data,&n);
+	}*/
 
 	/* use your functions there*/
 
